e9f9218098dfc018f60c10b98d3fe039530679e0
INSPIRON-7370\david <dwdq>
2023-05-10 18:45:44 +0200

    Bugfix of init and key schedule

--- project_ascon.srcs/sources_1/new/ascon_initalization.sv
+++ project_ascon.srcs/sources_1/new/ascon_initalization.sv
@@ -19,40 +19,41 @@
 // 
 //////////////////////////////////////////////////////////////////////////////////
 
-
-module ascon_initialization(
-    input clk,
-    input rst,
-    input [127:0] key,
-    input [127:0] nonce,
-    output reg [127:0] state_out
+module ascon_initialization (
+  input clk,
+  input rst,
+  input [127:0] key,
+  input [127:0] nonce,
+  output reg [127:0] state_out
 );
 
-    wire [127:0] round_const;
-    wire [127:0] temp_state;
-
-    // Compute the round constant
-    ascon_add_constant round_const_inst(
-        .round_num(0),
-        .state_in(0),
-        .state_out(round_const)
-    );
-
-    // Initialize the state with the nonce and the key
-    assign temp_state = {nonce, key};
-
-    // XOR the first part of the state with the round constant
-    assign state_out = round_const ^ temp_state;
-
-    // Register the output state on the positive edge of the clock
-    always @(posedge clk) begin
-        if (rst) begin
-            state_out <= 0;
-        end else begin
-            state_out <= temp_state;
-        end
+  wire [127:0] round_state;
+  reg [63:0] round_num;
+  
+  ascon_round round_inst (
+    .clk(clk),
+    .rst(rst),
+    .state_in({key, nonce}),
+    .state_out(round_state),
+    .round_number(round_num)
+  );
+  
+  always @(posedge clk or posedge rst) begin
+    if (rst) begin
+      round_num <= 0;
+      state_out <= 128'h0000000000000000_8080808080808080;
+    end else if (round_num < 12) begin
+      round_num <= round_num + 1;
+      state_out <= round_state;
     end
+  end
+
 endmodule
+
+
+
+
+
 
 
 
